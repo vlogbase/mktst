9613b67c20c02d5c038f51c9b58fb9e31d7836ba
emredemirel <emredemirel4196@gmail.com>
2022-01-30 01:13:52 +0300

    add gallery

--- resources/views/admin/contents/contents/gallery/gallery_list.blade.php
+++ resources/views/admin/contents/contents/gallery/gallery_list.blade.php
@@ -6,8 +6,57 @@
                     <div class="row gy-5 g-xl-8">
                         <!--begin::Col-->
                         <div class="col-xl-12">
-                                Gallery List
+                            <div class="card shadow-sm">
+                                <div class="card-header">
+                                    <h3 class="card-title">Add New Image</h3>
+                                </div>
+                                <div class="card-body">
+                                    <!--begin::Form-->
+                                    <form class="form" action="#" method="post">
+                                        <!--begin::Input group-->
+                                        <div class="fv-row">
+                                            <!--begin::Dropzone-->
+                                            <div class="dropzone" id="kt_dropzonejs_example_1">
+                                                <!--begin::Message-->
+                                                <div class="dz-message needsclick">
+                                                    <!--begin::Icon-->
+                                                    <i class="bi bi-file-earmark-arrow-up text-primary fs-3x"></i>
+                                                    <!--end::Icon-->
+
+                                                    <!--begin::Info-->
+                                                    <div class="ms-4">
+                                                        <h3 class="fs-5 fw-bolder text-gray-900 mb-1">Drop files here or click to upload.</h3>
+                                                        <span class="fs-7 fw-bold text-gray-400">Upload up to 10 files</span>
+                                                    </div>
+                                                    <!--end::Info-->
+                                                </div>
+                                            </div>
+                                            <!--end::Dropzone-->
+                                        </div>
+                                        <!--end::Input group-->
+                                    </form>
+                                    <!--end::Form-->
+                                </div>
+                            </div>
                         </div>
                         <!--end::Col-->
                     </div>
+@endsection
+@section('js')
+ <script>
+     var myDropzone = new Dropzone("#kt_dropzonejs_example_1", {
+    url: "https://keenthemes.com/scripts/void.php", // Set the url for your upload script location
+    paramName: "file", // The name that will be used to transfer the file
+    maxFiles: 10,
+    maxFilesize: 10, // MB
+    addRemoveLinks: true,
+    accept: function(file, done) {
+        if (file.name == "wow.jpg") {
+            done("Naha, you don't.");
+        } else {
+            done();
+        }
+    }
+});
+ </script>
 @endsection
