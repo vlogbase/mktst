c376ac1de68a0d1bc1d650c53f898c9b9607171d
emre demirel <emredemire4196@gmail.com>
2023-04-10 08:11:12 +0300

    open the pseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(ProductSeeder::class);
+        $this->call(ProductSeeder::class);
         $this->call(ContentSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(OtherSeeder::class);
