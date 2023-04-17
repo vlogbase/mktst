93b769344a0f13388b2f749d6ab121ed226cb71f
emre demirel <emredemire4196@gmail.com>
2023-04-18 02:37:31 +0300

    fix smth

--- database/seeders/ContentSeeder.php
+++ database/seeders/ContentSeeder.php
@@ -5,8 +5,10 @@ namespace Database\Seeders;
 use App\Models\AppSlider;
 use App\Models\Blog;
 use App\Models\Gallery;
+use App\Models\JobResume;
 use App\Models\Message;
 use App\Models\News;
+use App\Models\Newsletter;
 use App\Models\WebSlider;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -27,12 +29,17 @@ class ContentSeeder extends Seeder
         WebSlider::truncate();
         AppSlider::truncate();
         //Message::truncate();
+        //Newsletter::truncate();
+        //JobResume::truncate();
 
+
+        //JobResume::factory(20)->create();
+        //Newsletter::factory(20)->create();
         //Gallery::factory(20)->create();
         //Blog::factory(20)->create();
         //News::factory(20)->create();
-        WebSlider::factory(3)->create();
-        AppSlider::factory(3)->create();
+        WebSlider::factory(1)->create();
+        AppSlider::factory(1)->create();
         //Message::factory(20)->create();
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
