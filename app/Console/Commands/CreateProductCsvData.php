88e732e0a5737e73ca9ce192dd2b41cd34a3635f
Doğukan Emre Demirel <emredemirel4196@gmail.com>
2024-02-08 05:05:58 +0300

    wip

--- app/Console/Commands/CreateProductCsvData.php
+++ app/Console/Commands/CreateProductCsvData.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Product;
 use Illuminate\Console\Command;
 
 class CreateProductCsvData extends Command
@@ -37,6 +38,40 @@ class CreateProductCsvData extends Command
      */
     public function handle()
     {
-        return 0;
+        $csvFileName = 'result_data.csv';
+
+        // Dosyayı yazma modunda aç
+        $fp = fopen($csvFileName, 'w');
+
+        // İsteğe bağlı: CSV dosyasının başlığını ekle
+        fputcsv($fp, ['SKU', 'Name', 'Tax', 'Unit Price', 'Unit Per Price', 'Brand Name', 'Pack Size', 'Category', 'Sub Category']);
+
+                $products = Product::all();
+                foreach ($products as $product) {
+                    $category = $product->categories->where('category_id', null)->first();
+                    if(!is_null($category)){
+                        $sub_category = $product->categories->where('category_id', $category->id)->first();
+                    }else{
+                        $sub_category = null;
+                    }
+                    
+
+                    fputcsv($fp, [
+                        $product->sku,
+                        $product->name,
+                        $product->taxrate,
+                        floatval($product->unit_price),
+                        floatval($product->unit_per_price),
+                        $product->brand->name,
+                        $product->productdetail->pack,
+                        $category ? $category->name : '',
+                        $sub_category ? $sub_category->name : '',
+                    ]);
+        }
+        // Dosyayı kapat
+        fclose($fp);
+
+        return;
     }
+
 }
