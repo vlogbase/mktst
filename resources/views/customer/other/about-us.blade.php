44a6261774b7e48f70a650d4f6bc9e8d1d621f36
Doğukan Emre Demirel <emredemirel4196@gmail.com>
2024-04-15 17:09:13 +0300

    wip

--- resources/views/customer/other/about-us.blade.php
+++ resources/views/customer/other/about-us.blade.php
@@ -33,6 +33,9 @@
                     </div>
                     <div class="col-lg-6 order-2 order-lg-2">
                         <div class="px-5">
+                            <p>Markket was established in 1985, it has grown over the years from a small corner outlet, to a
+                                well-established state of the art multi temperature and multi cultural foodservice
+                                distributor.</p>
                             <p>We supply a diverse range of chilled and frozen foods as well as a comprehensive selection of
                                 retail, catering products and non-food items. Our products also include established brands
                                 as well as a competitively priced own label ranges, creating a ‘one-stop-shop’ for all our
@@ -47,10 +50,6 @@
                                 quality and better priced .We bring the world markets to your business. We also work with
                                 small artisan producers to bring unique quality products to you and the opportunity to build
                                 new product lines</p>
-                            <p>
-                                With the partnership of this network we continue to expand the enormous range of products
-                                and offerings that our clients value, and we bring to our clients the quality of products
-                                that they have come to expect from Markket</p>
                         </div>
                     </div>
                     <div class="col-lg-6 order-1 order-lg-2 ">
@@ -68,25 +67,38 @@
                     </div>
                     <div class="col-lg-6 order-2 order-lg-2">
                         <div class="px-5">
-                            <p>With over 13500 products in our range, Markket offer our customers a diversity of choice and
-                                the
-                                availability of a ‘one-stop-shop’ for all catering supplies. Professional and correct food
-                                handling, along with health and safety, are of paramount importance at Markket.</p>
+                            <p>
+                                With the partnership of this network we continue to expand the enormous range of products
+                                and offerings that our clients value, and we bring to our clients the quality of products
+                                that they have come to expect from Markket</p>
                         </div>
                     </div>
                 </div>
                 <div class="row align-items-center pt-5 pb-5 mt-5 mb-5">
                     <div class="col-lg-6 order-2 order-lg-1">
                         <div class="px-5">
-                        <p>Markket deliver on 24 hour lead time (Offshore deliveries may vary) and offer a full telesales
-                            service up to 5pm as well as a 24 hour online ordering platform. We understand the needs our
-                            clients have outside of the normal working week and therefore we also provide a Saturday
-                            delivery.</p>
+                            <p>With over 13500 products in our range, Markket offer our customers a diversity of choice and
+                                the
+                                availability of a ‘one-stop-shop’ for all catering supplies. Professional and correct food
+                                handling, along with health and safety, are of paramount importance at Markket.</p>
                         </div>
                     </div>
                     <div class="col-lg-6 order-1 order-lg-2">
                         <div class="about_img scene mb-4 mb-lg-0">
                             <img style="box-shadow: 10px 10px 20px rgba(0,0,0,0.50);" src="customer_assets/images/warehouse-aisle-ikea-store.jpg" alt="about_img" />
+                        </div>
+                    </div>
+                </div>
+                <div class="row align-items-center mt-5 mb-5 pt-5 pb-5">
+                    <div class="col-lg-6 order-1 order-lg-1">
+                        <div class="about_img scene mb-4 mb-lg-0">
+                            <img style="box-shadow: 10px 10px 20px rgba(0,0,0,0.50);" src="customer_assets/images/colleague-using-digital-equipment.jpg"
+                                alt="about_img" />
+                        </div>
+                    </div>
+                    <div class="col-lg-6 order-2 order-lg-2">
+                        <div class="px-5">
+                            <p>Markket delivers on a 24-hour lead time (Offshore deliveries may vary) and offers a full 24-hour online ordering platform. We understand the needs our clients have outside of the normal working week, and therefore, we also provide a Saturday delivery.</p>
                         </div>
                     </div>
                 </div>
