e08cd5956f6583f2d0fbe9eab8673d6b6f70598a
Doğukan Emre Demirel <emredemirel4196@gmail.com>
2024-04-15 13:07:08 +0300

    wip about

--- resources/views/customer/other/about-us.blade.php
+++ resources/views/customer/other/about-us.blade.php
@@ -26,13 +26,10 @@
             <div class="row align-items-center  mb-5">
                 <div class="col-lg-6 order-1 order-lg-1">
                     <div class="about_img scene mb-4 mb-lg-0">
-                        <img src="upload/other/aboutus1.png" alt="about_img"/>
+                        <img src="customer_assets/images/cold-storage-warehouse-with-stacks-crates-containing-fresh-produce-generative-ai.jpg" alt="about_img"/>
                     </div>
                 </div>
                 <div class="col-lg-6 order-2 order-lg-2">
-                    <div class="heading_s1">
-                        <h2>WHY CHOOSE Markket</h2>
-                    </div>
                     <p>Markket was established in 1985, it has grown over the years from a small corner outlet, to a well-established state of the art multi temperature and multi cultural foodservice distributor.</p>
                     <p>We supply a diverse range of chilled and frozen foods as well as a comprehensive selection of retail, catering products and non-food items. Our products also include established brands as well as a competitively priced own label ranges, creating a ‘one-stop-shop’ for all our clients.</p>
 
@@ -42,22 +39,31 @@
                 <div class="col-lg-6 order-2 order-lg-1">
                     <p>
                         With the partnership of this network we continue to expand the enormous range of products and offerings that our clients value, and we bring to our clients the quality of products that they have come to expect from Markket</p>
-                        <p>With over 13500 products in our range, Markket offer our customers a diversity of choice and the availability of a ‘one-stop-shop’ for all catering supplies. Professional and correct food handling, along with health and safety, are of paramount importance at Markket.</p>
                     </div>
                 <div class="col-lg-6 order-1 order-lg-2">
                     <div class="about_img scene mb-4 mb-lg-0">
-                        <img src="upload/other/savoyabout2.png" alt="about_img"/>
+                        <img src="customer_assets/images/stack-multi-colored-pencils-store.jpg" alt="about_img"/>
                     </div>
                 </div>
             </div>
             <div class="row align-items-center mt-5 mb-5">
                 <div class="col-lg-6 order-1 order-lg-1">
                     <div class="about_img scene mb-4 mb-lg-0">
-                        <img src="upload/other/generic-warehouse.jpg" alt="about_img"/>
+                        <img src="customer_assets/images/man-looking-away-while-standing-supermarket.jpg" alt="about_img"/>
                     </div>
                 </div>
                 <div class="col-lg-6 order-2 order-lg-2">
+                    <p>With over 13500 products in our range, Markket offer our customers a diversity of choice and the availability of a ‘one-stop-shop’ for all catering supplies. Professional and correct food handling, along with health and safety, are of paramount importance at Markket.</p>
+                </div>
+            </div>
+            <div class="row align-items-center mt-5 mb-5">
+                <div class="col-lg-6 order-2 order-lg-1">
                     <p>Markket deliver on 24 hour lead time (Offshore deliveries may vary) and offer a full telesales service up to 5pm as well as a 24 hour online ordering platform. We understand the needs our clients have outside of the normal working week and therefore we also provide a Saturday delivery.</p>
+                    </div>
+                <div class="col-lg-6 order-1 order-lg-2">
+                    <div class="about_img scene mb-4 mb-lg-0">
+                        <img src="customer_assets/images/warehouse-aisle-ikea-store.jpg" alt="about_img"/>
+                    </div>
                 </div>
             </div>
         </div>
