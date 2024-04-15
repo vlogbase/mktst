003c322be27f40800ee35b2d42ef9c5aa84fac39
Doğukan Emre Demirel <emredemirel4196@gmail.com>
2024-04-15 16:25:40 +0300

    wip shadow

--- resources/views/customer/other/about-us.blade.php
+++ resources/views/customer/other/about-us.blade.php
@@ -1,128 +1,155 @@
 @extends('customer.layouts.master')
-@section('title','Why Choose Markket - Markket')
+@section('title', 'Why Choose Markket - Markket')
 @section('content')
-<!-- START SECTION BREADCRUMB -->
-<div class="breadcrumb_section  page-title-mini" style="background-color:chocolate;">
-    <div class="container"><!-- STRART CONTAINER -->
-        <div class="row align-items-center">
-        	<div class="col-md-12 text-center">
-                <div class="page-title">
-            		<h1 style="color:white!important;">Why Choose Markket</h1>
+    <!-- START SECTION BREADCRUMB -->
+    <div class="breadcrumb_section  page-title-mini" style="background-color:chocolate;">
+        <div class="container"><!-- STRART CONTAINER -->
+            <div class="row align-items-center">
+                <div class="col-md-12 text-center">
+                    <div class="page-title">
+                        <h1 style="color:white!important;">Why Choose Markket</h1>
+                    </div>
                 </div>
-            </div>
-            
-        </div>
-    </div><!-- END CONTAINER-->
-</div>
-<!-- END SECTION BREADCRUMB -->
 
-
-<!-- START MAIN CONTENT -->
-<div class="main_content">
-
-    <!-- STAT SECTION ABOUT --> 
-    <div class="section">
-        <div class="container">
-            <div class="row align-items-center  mb-5">
-                <div class="col-lg-6 order-1 order-lg-1">
-                    <div class="about_img scene mb-4 mb-lg-0">
-                        <img src="customer_assets/images/cold-storage-warehouse-with-stacks-crates-containing-fresh-produce-generative-ai.jpg" alt="about_img"/>
-                    </div>
-                </div>
-                <div class="col-lg-6 order-2 order-lg-2">
-                    <p>Markket was established in 1985, it has grown over the years from a small corner outlet, to a well-established state of the art multi temperature and multi cultural foodservice distributor.</p>
-                    <p>We supply a diverse range of chilled and frozen foods as well as a comprehensive selection of retail, catering products and non-food items. Our products also include established brands as well as a competitively priced own label ranges, creating a ‘one-stop-shop’ for all our clients.</p>
-
-                </div>
             </div>
-            <div class="row align-items-center mt-5 mb-5">
-                <div class="col-lg-6 order-2 order-lg-1">
-                    <p>
-                        With the partnership of this network we continue to expand the enormous range of products and offerings that our clients value, and we bring to our clients the quality of products that they have come to expect from Markket</p>
-                    </div>
-                <div class="col-lg-6 order-1 order-lg-2">
-                    <div class="about_img scene mb-4 mb-lg-0">
-                        <img src="customer_assets/images/stack-multi-colored-pencils-store.jpg" alt="about_img"/>
-                    </div>
-                </div>
-            </div>
-            <div class="row align-items-center mt-5 mb-5">
-                <div class="col-lg-6 order-1 order-lg-1">
-                    <div class="about_img scene mb-4 mb-lg-0">
-                        <img src="customer_assets/images/man-looking-away-while-standing-supermarket.jpg" alt="about_img"/>
-                    </div>
-                </div>
-                <div class="col-lg-6 order-2 order-lg-2">
-                    <p>With over 13500 products in our range, Markket offer our customers a diversity of choice and the availability of a ‘one-stop-shop’ for all catering supplies. Professional and correct food handling, along with health and safety, are of paramount importance at Markket.</p>
-                </div>
-            </div>
-            <div class="row align-items-center mt-5 mb-5">
-                <div class="col-lg-6 order-2 order-lg-1">
-                    <p>Markket deliver on 24 hour lead time (Offshore deliveries may vary) and offer a full telesales service up to 5pm as well as a 24 hour online ordering platform. We understand the needs our clients have outside of the normal working week and therefore we also provide a Saturday delivery.</p>
-                    </div>
-                <div class="col-lg-6 order-1 order-lg-2">
-                    <div class="about_img scene mb-4 mb-lg-0">
-                        <img src="customer_assets/images/warehouse-aisle-ikea-store.jpg" alt="about_img"/>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div><!-- END CONTAINER-->
     </div>
-    <!-- END SECTION ABOUT --> 
+    <!-- END SECTION BREADCRUMB -->
 
 
-<!-- START SECTION CONTACT -->
-<div class="section">
-	<div class="container">
-        <div class="row align-items-center ">
-            <div class="col-lg-12 ">
-                <div class="heading_s1">
-                    <h2>COMPANY INFORMATIONS</h2>
+    <!-- START MAIN CONTENT -->
+    <div class="main_content">
+
+        <!-- STAT SECTION ABOUT -->
+        <div class="section">
+            <div class="container">
+                <div class="row align-items-center pb-5 mb-5">
+                    <div class="col-lg-6 order-1 order-lg-1">
+                        <div class="about_img scene mb-4 mb-lg-0">
+                            <img style="box-shadow: 10px 10px 20px rgba(0,0,0,0.50);"
+                                src="customer_assets/images/cold-storage-warehouse-with-stacks-crates-containing-fresh-produce-generative-ai.jpg"
+                                alt="about_img" />
+                        </div>
+                    </div>
+                    <div class="col-lg-6 order-2 order-lg-2">
+                        <div class="px-5">
+                            <p>Markket was established in 1985, it has grown over the years from a small corner outlet, to a
+                                well-established state of the art multi temperature and multi cultural foodservice
+                                distributor.</p>
+                            <p>We supply a diverse range of chilled and frozen foods as well as a comprehensive selection of
+                                retail, catering products and non-food items. Our products also include established brands
+                                as well as a competitively priced own label ranges, creating a ‘one-stop-shop’ for all our
+                                clients.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row align-items-center mt-5 mb-5 pt-5 pb-5">
+                    <div class="col-lg-6 order-2 order-lg-1">
+                        <div class="px-5">
+                            <p>Markket is an importer of leading products from top producers from all over the globe high
+                                quality and better priced .We bring the world markets to your business. We also work with
+                                small artisan producers to bring unique quality products to you and the opportunity to build
+                                new product lines</p>
+                            <p>
+                                With the partnership of this network we continue to expand the enormous range of products
+                                and offerings that our clients value, and we bring to our clients the quality of products
+                                that they have come to expect from Markket</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 order-1 order-lg-2 ">
+                        <div class="about_img scene mb-4 mb-lg-0">
+                            <img style="box-shadow: 10px 10px 20px rgba(0,0,0,0.50);" src="customer_assets/images/stack-multi-colored-pencils-store.jpg" alt="about_img" />
+                        </div>
+                    </div>
+                </div>
+                <div class="row align-items-center mt-5 mb-5 pt-5 pb-5">
+                    <div class="col-lg-6 order-1 order-lg-1">
+                        <div class="about_img scene mb-4 mb-lg-0">
+                            <img style="box-shadow: 10px 10px 20px rgba(0,0,0,0.50);" src="customer_assets/images/man-looking-away-while-standing-supermarket.jpg"
+                                alt="about_img" />
+                        </div>
+                    </div>
+                    <div class="col-lg-6 order-2 order-lg-2">
+                        <div class="px-5">
+                            <p>With over 13500 products in our range, Markket offer our customers a diversity of choice and
+                                the
+                                availability of a ‘one-stop-shop’ for all catering supplies. Professional and correct food
+                                handling, along with health and safety, are of paramount importance at Markket.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row align-items-center pt-5 pb-5 mt-5 mb-5">
+                    <div class="col-lg-6 order-2 order-lg-1">
+                        <div class="px-5">
+                        <p>Markket deliver on 24 hour lead time (Offshore deliveries may vary) and offer a full telesales
+                            service up to 5pm as well as a 24 hour online ordering platform. We understand the needs our
+                            clients have outside of the normal working week and therefore we also provide a Saturday
+                            delivery.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 order-1 order-lg-2">
+                        <div class="about_img scene mb-4 mb-lg-0">
+                            <img style="box-shadow: 10px 10px 20px rgba(0,0,0,0.50);" src="customer_assets/images/warehouse-aisle-ikea-store.jpg" alt="about_img" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row align-items-center">
-            <div class="col-xl-3 col-md-6">
-            	<div class="contact_wrap contact_style3">
-                    <div class="contact_text">
-                        <span>Register No</span>
-                        <p>01888960</p>
-                        <p></p>
+        <!-- END SECTION ABOUT -->
+
+
+        <!-- START SECTION CONTACT -->
+        <div class="section">
+            <div class="container">
+                <div class="row align-items-center ">
+                    <div class="col-lg-12 ">
+                        <div class="heading_s1">
+                            <h2>COMPANY INFORMATIONS</h2>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-            	<div class="contact_wrap contact_style3">
-                    <div class="contact_text">
-                        <span>VAT</span>
-                        <p>426 1571 64</p>
-                        <p></p>
+                <div class="row align-items-center">
+                    <div class="col-xl-3 col-md-6">
+                        <div class="contact_wrap contact_style3">
+                            <div class="contact_text">
+                                <span>Register No</span>
+                                <p>01888960</p>
+                                <p></p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-            	<div class="contact_wrap contact_style3">
-                    <div class="contact_text">
-                        <span>EORI Register No</span>
-                        <p>GB 426 1571 64 000 
-                        </p>
-                        <p></p>
+                    <div class="col-xl-3 col-md-6">
+                        <div class="contact_wrap contact_style3">
+                            <div class="contact_text">
+                                <span>VAT</span>
+                                <p>426 1571 64</p>
+                                <p></p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-            	<div class="contact_wrap contact_style3">
-                    <div class="contact_text">
-                        <span>Company Name</span>
-                        <p>Integrated Brand Delivery Ltd</p>
+                    <div class="col-xl-3 col-md-6">
+                        <div class="contact_wrap contact_style3">
+                            <div class="contact_text">
+                                <span>EORI Register No</span>
+                                <p>GB 426 1571 64 000
+                                </p>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6">
+                        <div class="contact_wrap contact_style3">
+                            <div class="contact_text">
+                                <span>Company Name</span>
+                                <p>Integrated Brand Delivery Ltd</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- END SECTION CONTACT -->
+
     </div>
-</div>
-<!-- END SECTION CONTACT -->
-
-</div>
-<!-- END MAIN CONTENT -->   
+    <!-- END MAIN CONTENT -->
 @endsection
