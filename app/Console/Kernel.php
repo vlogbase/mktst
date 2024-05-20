eb9a411f34d22ee5bc31806dfc6b5fc6e4d73bc7
anuragroopchandani <anuragroopchandani@gmail.com>
2024-05-20 22:45:14 +0530

    Feature: Automated message for expiring key

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Console;
 
+use App\Models\Admin;
+use App\Models\Seller;
+use App\Notifications\ApiKeyExpireNotification;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +20,17 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        //schedule daily email to admins and sellers table for valid_till date less than 7 days and is_autoextended = 0
+        $schedule->call(function () {
+            $admins = Admin::where('valid_till', '<', Carbon::now()->addDays(7))->where('is_autoextended', 0)->get();
+            foreach ($admins as $admin) {
+                $admin->notify(new ApiKeyExpireNotification($admin));
+            }
+            $sellers = Seller::where('valid_till', '<', Carbon::now()->addDays(7))->where('is_autoextended', 0)->get();
+            foreach ($sellers as $seller) {
+                $seller->notify(new ApiKeyExpireNotification($seller));
+            }
+        })->daily();
     }
 
     /**
